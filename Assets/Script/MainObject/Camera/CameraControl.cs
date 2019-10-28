3da78698a78a7e5236d7ec2f65f85176b3ee38f2
OpticalSpring <54758407+OpticalSpring@users.noreply.github.com>
2019-10-28 22:05:07 +0900

    플레이어 기본이동

---
+++ Assets/Script/MainObject/Camera/CameraControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float followSpeed;
+    public float rotationSpeed;
+    public float maxDistance;
+    float camDistance;
+    GameObject Player;
+    GameObject cam;
+    GameObject realCamera;
+    GameObject rotatePivot;
+    Vector3 rotateValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        cam = gameObject.transform.GetChild(0).gameObject;
+        realCamera = gameObject.transform.GetChild(0).GetChild(0).gameObject;
+        rotatePivot = gameObject.transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+        DistanceCheck();
+        DistanceSet();
+        RotatePosSet();
+        RotateCam();
+    }
+    
+    void RotatePosSet()
+    {
+        rotatePivot.transform.localEulerAngles = new Vector3(0, cam.transform.localEulerAngles.y, 0);
+    }
+
+    void Follow()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, Player.transform.position, followSpeed * Time.deltaTime);
+    }
+
+    void DistanceCheck()
+    {
+        RaycastHit rayHit;
+        
+        Debug.DrawRay(cam.transform.position, -cam.transform.forward * maxDistance, Color.red);
+        int mask = 1 << 9;
+        mask = ~mask;
+        if (Physics.SphereCast(cam.transform.position, 0.3f, -cam.transform.forward, out rayHit, maxDistance, mask))
+        {
+            Vector3 hitPoint = rayHit.point;
+            camDistance = Vector3.Distance(hitPoint, cam.transform.position) - 1f;
+            Debug.Log(rayHit.collider.name);
+        }
+        else
+        {
+            camDistance = maxDistance;
+        }
+    }
+    
+    void DistanceSet()
+    {
+        realCamera.transform.localPosition = new Vector3(1, 0, -camDistance);
+    }
+
+    void RotateCam()
+    {
+        Quaternion targetRotation;
+
+        rotateValue.y += Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+        rotateValue.x -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+        rotateValue.x = Mathf.Clamp(rotateValue.x, -30f, 70f);
+        targetRotation = Quaternion.Euler(rotateValue);
+
+        Quaternion q = targetRotation;
+        cam.transform.rotation = targetRotation;
+    }
+}
---
+++ Assets/Script/MainObject/Player/PlayerControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float horizontalValue;
+    public float VerticalValue;
+    GameObject rotatePos;
+    GameObject playerModel;
+    Vector3 targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotatePos = GameObject.Find("RotatePos");
+        playerModel = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputCheck();
+    }
+
+    void InputCheck()
+    {
+        horizontalValue = Input.GetAxisRaw("Horizontal");
+        VerticalValue = Input.GetAxisRaw("Vertical");
+
+
+
+        if (horizontalValue != 0 || VerticalValue != 0)
+        {
+            TurnPointCheck();
+            Turn(playerModel, targetPos);
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    void TurnPointCheck()
+    {
+        rotatePos.transform.localPosition = new Vector3(horizontalValue * 1000, 0, VerticalValue * 1000);
+        targetPos = rotatePos.transform.position;
+    }
+
+    void Turn(GameObject obj, Vector3 target)
+    {
+        float dz = target.z - obj.transform.position.z;
+        float dx = target.x - obj.transform.position.x;
+
+        float rotateDegree = Mathf.Atan2(dx, dz) * Mathf.Rad2Deg;
+
+        obj.transform.rotation = Quaternion.RotateTowards(obj.transform.rotation, Quaternion.Euler(0, rotateDegree, 0), rotateSpeed * Time.deltaTime);
+
+    }
+}
