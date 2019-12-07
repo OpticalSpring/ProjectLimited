8870acf44834e5c080b6a69cf7bc6611b4637bd9
OpticalSpring <54758407+OpticalSpring@users.noreply.github.com>
2019-12-08 02:56:43 +0900

    열차가 움직여요

---
+++ Assets/Script/ETC/Subway.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subway : MonoBehaviour
+{
+    public int num;
+    public GameObject checkPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(gameObject.transform.position, checkPoint.transform.GetChild(num).position) < 1)
+        {
+            num++;
+            if(num >= checkPoint.transform.childCount)
+            {
+                num = 0;
+                gameObject.transform.position = checkPoint.transform.GetChild(0).position;
+            }
+        }
+        else
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, checkPoint.transform.GetChild(num).position, Time.deltaTime * 50);
+            gameObject.transform.LookAt(checkPoint.transform.GetChild(num).position);
+        }
+    }
+}
